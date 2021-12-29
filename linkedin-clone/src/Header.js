3ea7b7387f6832d9a58cad0577a6cdeaeff4bb6f
Teddy <leclercqted@gmail.com>
2021-12-29 20:03:06 +0100

    [EOD] - Header and Sidebar

---
+++ linkedin-clone/src/Header.js
@@ -0,0 +1,39 @@
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import LinkedinIcon from './img/linkedin.png'
+import { Avatar } from "@material-ui/core"
+import HeaderOption from './HeaderOption';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img
+                    src={LinkedinIcon}
+                    alt=""
+                />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title='Home' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption avatar={Avatar} title='me' />
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
